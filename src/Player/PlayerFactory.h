bf628a474d9d22735ed68c59d523c32a89f30d2c
GRAnimated <devin@nhscan.com>
2026-03-08 01:33:40 -0500

    StageScene::init: inline PlayerFactory::createActor (80.7% → 82.3%)

--- src/Player/PlayerFactory.h
+++ src/Player/PlayerFactory.h
@@ -13,5 +13,14 @@ using PlayerCreatorFunction = al::LiveActor* (*)(const char* actorName);
 class PlayerFactory : public al::Factory<PlayerCreatorFunction> {
 public:
     PlayerFactory();
-    al::LiveActor* createActor(const al::ActorInitInfo& initInfo, const char* className);
+
+    al::LiveActor* createActor(const al::ActorInitInfo& initInfo, const char* className) {
+        PlayerCreatorFunction creationFunc = nullptr;
+        getEntryIndex(&creationFunc, className);
+        if (!creationFunc)
+            return nullptr;
+        const char* displayName = nullptr;
+        al::getDisplayName(&displayName, initInfo);
+        return creationFunc(displayName);
+    }
 };
