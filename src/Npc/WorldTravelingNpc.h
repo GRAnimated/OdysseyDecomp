8fab0e2247b46cc5bde3270a656733edf4f59a8f
GRAnimated <devin@nhscan.com>
2026-03-09 20:28:53 -0400

    feat: implement WorldTravelingNpc

--- src/Npc/WorldTravelingNpc.h
+++ src/Npc/WorldTravelingNpc.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <prim/seadSafeString.h>
+
 #include "Library/Event/IEventFlowEventReceiver.h"
 #include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+struct ActorInitInfo;
+class EventFlowExecutor;
+class LiveActorGroup;
+}  // namespace al
+
+class TalkNpcActionAnimInfo;
+class TalkNpcParam;
+class TalkNpcStateEvent;
+class WorldTravelingPartner;
 
 class WorldTravelingNpc : public al::LiveActor, public al::IEventFlowEventReceiver {
 public:
@@ -11,12 +24,17 @@ public:
     void attackSensor(al::HitSensor* self, al::HitSensor* other) override;
     bool receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
                     al::HitSensor* self) override;
-    bool receiveEvent(const al::EventFlowEventData* event) override;
+    bool receiveEvent(const al::EventFlowEventData* data) override;
 
     void exeEvent();
 
-private:
-    void* _padding[0x11];
+    al::EventFlowExecutor* mEventFlowExecutor = nullptr;
+    TalkNpcStateEvent* mTalkNpcStateEvent = nullptr;
+    TalkNpcParam* mTalkNpcParam = nullptr;
+    TalkNpcActionAnimInfo* mTalkNpcActionAnimInfo = nullptr;
+    WorldTravelingPartner* mPartner = nullptr;
+    al::LiveActorGroup* mCarGroup = nullptr;
+    sead::WFixedSafeString<32> mCharacterName;
 };
 
 static_assert(sizeof(WorldTravelingNpc) == 0x198);
