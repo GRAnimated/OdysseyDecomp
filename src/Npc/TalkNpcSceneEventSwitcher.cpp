748438ded59cd52867735a724cae7cea7e2ba773
GRAnimated <devin@nhscan.com>
2026-03-09 20:27:59 -0400

    feat: implement TalkNpcSceneEventSwitcher

---
+++ src/Npc/TalkNpcSceneEventSwitcher.cpp
@@ -0,0 +1,7 @@
+#include "Npc/TalkNpcSceneEventSwitcher.h"
+
+TalkNpcSceneEventSwitcher::TalkNpcSceneEventSwitcher() = default;
+
+const char* TalkNpcSceneEventSwitcher::getSceneObjName() const {
+    return "TalkNpcSceneEventSwitcher";
+}
