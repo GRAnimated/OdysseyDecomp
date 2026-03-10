88ca2a65fa18e5274104686bb64f51acd34df23a
GRAnimated <devin@nhscan.com>
2026-03-09 20:27:49 -0400

    feat: implement PeachOnKoopaAnimInfo

---
+++ src/Npc/PeachOnKoopaAnimInfo.cpp
@@ -0,0 +1,29 @@
+#include "Npc/PeachOnKoopaAnimInfo.h"
+
+#include "Library/Base/StringUtil.h"
+
+PeachOnKoopaAnimInfo::PeachOnKoopaAnimInfo() = default;
+
+void PeachOnKoopaAnimInfo::update(const char* body, const char* leftHand, const char* rightHand) {
+    mIsBodyChanged = false;
+    mIsLeftHandChanged = false;
+    mIsRightHandChanged = false;
+
+    if ((body == nullptr) != (mBodyAnim == nullptr) ||
+        (body && mBodyAnim && !al::isEqualString(body, mBodyAnim))) {
+        mIsBodyChanged = true;
+        mBodyAnim = body;
+    }
+
+    if ((leftHand == nullptr) != (mLeftHandAnim == nullptr) ||
+        (leftHand && mLeftHandAnim && !al::isEqualString(leftHand, mLeftHandAnim))) {
+        mIsLeftHandChanged = true;
+        mLeftHandAnim = leftHand;
+    }
+
+    if ((rightHand == nullptr) != (mRightHandAnim == nullptr) ||
+        (rightHand && mRightHandAnim && !al::isEqualString(rightHand, mRightHandAnim))) {
+        mIsRightHandChanged = true;
+        mRightHandAnim = rightHand;
+    }
+}
